60899b50339790c3fb9199684a176741e7f1dc51
noasignado <noasignado@DESKTOP-NJL43H5>
2021-01-20 20:50:22 -0500

    prueba de eclipse 2

---
+++ ProbandoCargue/src/com/co/cargue/IPersona.java
@@ -0,0 +1,7 @@
+package com.co.cargue;
+
+public interface IPersona  {
+public String cargarDatos(Persona persona);
+public boolean  existeDatos(Integer id);
+public void deleteDatos(Integer id);
+}
